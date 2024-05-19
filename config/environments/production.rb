1761407b6fe66039273f66475c710b0f929fbc48
Veldmord <egor-1408@mail.ru>
2024-05-19 15:42:51 +0300

    123

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,8 @@ Rails.application.configure do
   config.cache_classes = true
 
   
-  config.active_record.adapter = :postgresql
+  #config.active_record.adapter = :postgresql
+  
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
