1515a2c545a02d9346ec5171106ddecd83dbd2d7
Veldmord <egor-1408@mail.ru>
2024-05-21 15:56:20 +0300

    prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-
+  config.request_timeout = 300 # 5 минут
+  config.middleware.insert_before 0, Rack::Timeout, service_timeout: 300
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
